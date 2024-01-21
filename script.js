2cd9212b010dd6725597a7b706557f3fd4ed69d4
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-01-21 23:44:06 +0100

    Løst ukesoppgavene for JavaScript1 og div. styling

---
+++ script.js
@@ -0,0 +1,88 @@
+/* UKESOPPGAVER JAVASCRIPT 1 */
+// Oppgave 1
+function javascript1oppg1() {
+    document.write("Hallo verden!");
+}
+// Oppgave 2
+function javascript1oppg2() {
+    alert("Hallo verden!");
+}
+// Oppgave 3
+function javascript1oppg3(innlestData) {
+    document.write(innlestData);
+}
+// Oppgave 4
+function javascript1oppg4(innlestData) {
+    alert(innlestData);
+}
+// Oppgave 5
+function javascript1oppg5(innlestData) {
+    console.log(innlestData);
+}
+// Oppgave 6
+function javascript1oppg6(innlestData) {
+    innlestData = innlestData.fontcolor("blue");
+    document.write(innlestData);
+}
+// Oppgave 7
+function javascript1oppg7(innlestData) {
+    innlestData = innlestData.toUpperCase();
+    alert(innlestData);
+}
+// Oppgave 8
+function javascript1oppg8() {
+    const teller = 1;
+    console.log(teller);
+}
+// Oppgave 9
+let javascript1teller = 0;
+function javascript1oppg9() {
+    javascript1teller += 1;
+    console.log(javascript1teller);
+}
+// Oppgave 10
+function javascript1oppg10() {
+    console.log("Du trykket på oppgave 9 knappen "+javascript1teller+ " ganger");
+}
+
+
+/* UKESOPPGAVER JAVASCRIPT 2 */
+
+
+/* UKESOPPGAVER JAVASCRIPT 3 */
+
+
+/* UKESOPPGAVER JQUERY */
+
+
+/* UKESOPPGAVER SPRING 1 */
+
+
+/* UKESOPPGAVER SPRING 2 */
+
+
+/* UKESOPPGAVER BOOTSTRAP */
+
+
+/* UKESOPPGAVER NEDTREKKSLISTER */
+
+
+/* UKESOPPGAVER DATASTRUKTURER */
+
+
+/* UKESOPPGAVER GENERISKE DATATYPER */
+
+
+/* Ukesoppgaver SQL 1 */
+
+
+/* Ukesoppgaver SQL 2 */
+
+
+/* UKESOPPGAVER INPUT VALIDERING */
+
+
+/* UKESOPPGAVER SESJONER */
+
+
+/* UKESOPPGAVER SIKKERHET */
