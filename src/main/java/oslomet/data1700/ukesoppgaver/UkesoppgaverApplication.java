4e20176a42c845757a867a03130981cadd65e215
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-02-02 18:12:56 +0100

    Laget filstruktur med Spring og opprettet eget js dokument for hvert kap

---
+++ src/main/java/oslomet/data1700/ukesoppgaver/UkesoppgaverApplication.java
@@ -0,0 +1,13 @@
+package oslomet.data1700.ukesoppgaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkesoppgaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkesoppgaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oslomet/data1700/ukesoppgaver/UkesoppgaverApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.data1700.ukesoppgaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkesoppgaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
